1e4dd6c939e0cc91bf1af23d7faa5a0e8c13694d
Yuya Fukumoto <fuku@rockme.co.jp>
2018-10-19 00:30:01 +0900

    tapping term

--- keyboards/crkbd/keymaps/yfuku/config.h
+++ keyboards/crkbd/keymaps/yfuku/config.h
@@ -41,8 +41,8 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 #define PREVENT_STUCK_MODIFIERS
 #define TAPPING_FORCE_HOLD
-#define TAPPING_TERM 100
-
+#define TAPPING_TERM 200
+    
 #undef RGBLED_NUM
 #define RGBLIGHT_ANIMATIONS
 #define RGBLED_NUM 27

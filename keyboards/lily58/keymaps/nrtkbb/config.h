346644d14638215ad05ab1685bb70c21a85125aa
nrtkbb <nrtkbb@gmail.com>
2019-01-23 04:04:09 +0900

    update

--- keyboards/lily58/keymaps/nrtkbb/config.h
+++ keyboards/lily58/keymaps/nrtkbb/config.h
@@ -40,7 +40,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define IGNORE_MOD_TAP_INTERRUPT
 #define PREVENT_STUCK_MODIFIERS
 
-#define MOUSE_MAX_SPEED 3
+#define MOUSE_MAX_SPEED 1
 #define MOUSE_TIME_TO_MAX 30
 
 

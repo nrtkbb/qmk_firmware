ef3c8a87d65f413101edd51b798eed752d851b53
Yuya Fukumoto <fuku@rockme.co.jp>
2019-02-19 06:06:47 +0900

    claw50

--- keyboards/claw50/rev1/rev1.h
+++ keyboards/claw50/rev1/rev1.h
@@ -29,11 +29,11 @@
     { L00, L01, L02, L03, L04, L05, L06 }, \
     { L10, L11, L12, L13, L14, L15, L16 }, \
     { L20, L21, L22, L23, L24, L25, L26 }, \
-    { KC_NO, KC_NO, L30, L31, L32, L33 }, \
+    { KC_NO, KC_NO, KC_NO, L30, L31, L32, L33 }, \
     { R06, R05, R04, R03, R02, R01, R00 }, \
     { R16, R15, R14, R13, R12, R11, R10 }, \
-    { Rs6, R25, R24, R23, R22, R21, R20 }, \
-    { KC_NO, KC_NO, R33, R32, R31, R30 } \
+    { R26, R25, R24, R23, R22, R21, R20 }, \
+    { KC_NO, KC_NO, KC_NO, R33, R32, R31, R30 } \
   }
 
 #define LAYOUT_kc( \
@@ -44,7 +44,7 @@
   ) \
   LAYOUT( \
     KC_##L00, KC_##L01, KC_##L02, KC_##L03, KC_##L04, KC_##L05, KC_##L06,    KC_##R00, KC_##R01, KC_##R02, KC_##R03, KC_##R04, KC_##R05, KC_##R06, \
-    KC_##L10, KC_##L11, KC_##L12, KC_##L13, KC_##L14, KC_##L15, KC_##L06,    KC_##R10, KC_##R11, KC_##R12, KC_##R13, KC_##R14, KC_##R15, KC_##R16, \
-    KC_##L20, KC_##L21, KC_##L22, KC_##L23, KC_##L24, KC_##L25, KC_##L06,    KC_##R20, KC_##R21, KC_##R22, KC_##R23, KC_##R24, KC_##R25, KC_##R26, \
+    KC_##L10, KC_##L11, KC_##L12, KC_##L13, KC_##L14, KC_##L15, KC_##L16,    KC_##R10, KC_##R11, KC_##R12, KC_##R13, KC_##R14, KC_##R15, KC_##R16, \
+    KC_##L20, KC_##L21, KC_##L22, KC_##L23, KC_##L24, KC_##L25, KC_##L26,    KC_##R20, KC_##R21, KC_##R22, KC_##R23, KC_##R24, KC_##R25, KC_##R26, \
                                   KC_##L30, KC_##L31, KC_##L32, KC_##L33,    KC_##R30, KC_##R31, KC_##R32, KC_##R33 \
   )
